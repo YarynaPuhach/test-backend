d3eb1f4b2ddc7b6427dd12b52cc4126b7cb029ef
Ярина Пугач <puhachyaryna@gmail.com>
2024-07-23 17:49:36 +0200

    Initial commit

--- models/index.js
+++ models/index.js
@@ -12,20 +12,35 @@ import { DelegationMap } from './delegation.model.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Зчитування конфігураційного файлу
+// Read the configuration file
 const env = process.env.NODE_ENV || 'development';
 const configPath = path.join(__dirname, '..', 'config', 'config.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf8'))[env];
 
-// Налаштування Sequelize
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  host: config.host,
-  dialect: config.dialect,
-  dialectOptions: config.dialectOptions,
-  logging: false // Вимкнути логування SQL-запитів
-});
+// Setup Sequelize
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false // Disable logging of SQL queries
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    dialectOptions: config.dialectOptions,
+    logging: false // Disable logging of SQL queries
+  });
+}
 
-// Ініціалізація моделей
+// Initialize models
 ContractorMap(sequelize);
 EmployeeMap(sequelize);
 InvoiceMap(sequelize);
