acf1323394604426f96d2f2c62b87aab695914f3
Ярина Пугач <puhachyaryna@gmail.com>
2024-07-24 13:02:17 +0200

    changed tables

--- models/invoice.model.js
+++ models/invoice.model.js
@@ -25,8 +25,25 @@ export const InvoiceMap = (sequelize) => {
         key: 'id'
       }
     },
-    amount: {
+    amount: { // Kwota Netto
       type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    quantity: { // Ilość
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1 // Domyślna ilość
+    },
+    description: { // Opis
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    mpk: { // MPK
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    vatRate: { // VAT
+      type: DataTypes.DECIMAL(5, 2), // np. 23.00
       allowNull: false
     },
     date: {
